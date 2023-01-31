eaaa3dd8073b0fa9005d5bc5526871b7e9805235
Soumendu Nayak <guitar.soumendu@gmail.com>
2023-01-31 17:21:42 +0530

    Day 2 Completed

---
+++ lib/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String greet = 'Hi';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(child: Container(child: Text('$greet world'))),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/HomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
-          Material(child: Center(child: Container(child: Text('Hello world')))),
+      home: HomePage(),
     );
   }
 }
